345b7dea4f8fb0b656327f35df8346b4eb420de4
Ivo La Puma <ivolapuma@gmail.com>
2021-06-03 16:05:55 -0300

    preparando api cadastrar rastreador

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -4,17 +4,34 @@ const express = require('express');
 const app = express();
 // console.log(`app: ${typeof(app)} | constructor:${app.constructor.name}`);
 
+// use() que permite configurar alguns recursos do express
+// essas configurações vão permitir acessar o body do request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000 /*porta*/, ()=>console.log('Servidor rodando na porta 3000...'));
 
 // criando a 1a rota
 // rota '/'
-
 // get() define a rota e a função que deve ser executada...
 app.get(
     '/', // é a URL da rota
     (request, response) => {
         console.log('Rota principal chamada...');
         response.send('Servidor rodando, está tudo OK');
+    }
+);
+
+// criando rota para cadastrar rastreador
+// POST
+// rota '/rastreador'
+app.post(
+    '/rastreador',
+    (request, response) => {
+        console.log('Rota /rastreador chamada...');
+        console.log(`request.body: ${request.body}`);
+        console.log(request.body);
+        response.send('OK');
     }
 );
 
